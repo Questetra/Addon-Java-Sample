85a6e3f3fd4104397929d06a6cb3624e64611d0f
Hatanaka, Akihiro <hatanaka@questetra.com>
2021-05-07 22:05:52 +0900

    first commit

---
+++ src/main/java/com/questetra/bpms/addon/sample/Executor.java
@@ -0,0 +1,13 @@
+package com.questetra.bpms.addon.sample;
+
+import com.questetra.bpms.addon.ServiceTaskExecutor;
+
+public class Executor implements ServiceTaskExecutor {
+
+    @Override
+    public void execute() throws Exception {
+        // TODO Auto-generated method stub
+
+    }
+
+}
